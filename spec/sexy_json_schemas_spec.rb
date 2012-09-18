fde73ca50a4ed8ca327a49afa752f617a6199f5f
Donald Plummer <donald@cideasphere.com>
2012-09-18 11:15:02 -0700

    Refactor tests

--- spec/sexy_json_schemas_spec.rb
+++ spec/sexy_json_schemas_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
+JSONTestClass = Class.new {
+  include SexyJSONSchemas
+}
+
 describe SexyJSONSchemas do
   class Money
     include SexyJSONSchemas
@@ -36,76 +40,177 @@ describe SexyJSONSchemas do
     end
   end
 
-  class Product
-    include SexyJSONSchemas
-
-    schema "producto", :root_element => false do
-      string_property "name"
-
-      object_property "attributes", :required => true do
-        string_property "color", :enum => %w[red blue green]
+  describe "null properties" do
+    subject { Class.new(JSONTestClass) {
+      schema "void", :root_element => false do
+        null_property "nope"
       end
+    }}
 
-      number_property "weight"
-
-      boolean_property "is_preorder"
-
-      null_property "spaceghost"
-
-      any_property "whatever"
-
-      union_property "maybe", [:null, :boolean]
-
-      array_property "links", :string
-    end
+    its(:as_json) { should == {
+      "name" => "void",
+      "type" => "object",
+      "properties" => {
+        "nope" => {
+          "type" => "null"
+        }
+      }
+    }}
   end
 
-  describe Product do
-    subject { Product }
+  describe "string properties" do
+    subject { Class.new(JSONTestClass) {
+      schema "void", :root_element => false do
+        string_property "silly"
+      end
+    }}
 
-    its(:schema_name) { should == "producto" }
+    its(:as_json) { should == {
+      "name" => "void",
+      "type" => "object",
+      "properties" => {
+        "silly" => {
+          "type" => "string"
+        }
+      }
+    }}
+  end
 
-    its(:as_json) do
-      should == {
-        "name" => "producto",
-        "type" => "object",
-        "properties" => {
-          "name" => {
+  describe "number properties" do
+    subject { Class.new(JSONTestClass) {
+      schema "void", :root_element => false do
+        number_property "beast"
+      end
+    }}
+
+    its(:as_json) { should == {
+      "name" => "void",
+      "type" => "object",
+      "properties" => {
+        "beast" => {
+          "type" => "number"
+        }
+      }
+    }}
+  end
+
+  describe "integer properties" do
+    subject { Class.new(JSONTestClass) {
+      schema "void", :root_element => false do
+        integer_property "cromulent"
+      end
+    }}
+
+    its(:as_json) { should == {
+      "name" => "void",
+      "type" => "object",
+      "properties" => {
+        "cromulent" => {
+          "type" => "integer"
+        }
+      }
+    }}
+  end
+
+  describe "any properties" do
+    subject { Class.new(JSONTestClass) {
+      schema "void", :root_element => false do
+        any_property "enlightened"
+      end
+    }}
+
+    its(:as_json) { should == {
+      "name" => "void",
+      "type" => "object",
+      "properties" => {
+        "enlightened" => {
+          "type" => "any"
+        }
+      }
+    }}
+  end
+
+  describe "boolean properties" do
+    subject { Class.new(JSONTestClass) {
+      schema "void", :root_element => false do
+        boolean_property "robot"
+      end
+    }}
+
+    its(:as_json) { should == {
+      "name" => "void",
+      "type" => "object",
+      "properties" => {
+        "robot" => {
+          "type" => "boolean"
+        }
+      }
+    }}
+  end
+
+  describe "union properties" do
+    subject { Class.new(JSONTestClass) {
+      schema "void", :root_element => false do
+        union_property "teachers", [:string, :null]
+      end
+    }}
+
+    its(:as_json) { should == {
+      "name" => "void",
+      "type" => "object",
+      "properties" => {
+        "teachers" => {
+          "type" => ["string", "null"]
+        }
+      }
+    }}
+  end
+
+  describe "array properties" do
+    subject { Class.new(JSONTestClass) {
+      schema "void", :root_element => false do
+        array_property "colors", :string
+      end
+    }}
+
+    its(:as_json) { should == {
+      "name" => "void",
+      "type" => "object",
+      "properties" => {
+        "colors" => {
+          "type" => "array",
+          "items" => {
             "type" => "string"
-          },
-          "weight" => {
-            "type" => "number"
-          },
-          "is_preorder" => {
-            "type" => "boolean"
-          },
-          "spaceghost" => {
-            "type" => "null"
-          },
-          "whatever" => {
-            "type" => "any"
-          },
-          "maybe" => {
-            "type" => ['null', 'boolean']
-          },
-          "links" => {
-            "type" => "array",
-            "items" => {
-              "type" => "string"
-            }
-          },
-          "attributes" => {
-            "type" => "object",
-            "required" => true,
-            "properties" => {
-              "color" => {
-                "type" => "string",
-                "enum" => %w[red blue green]
-              }
+          }
+        }
+      }
+    }}
+  end
+
+  describe "object properties" do
+    subject { Class.new(JSONTestClass) {
+      schema "void", :root_element => false do
+        object_property "attributes", :required => true do
+          string_property "color", :enum => %w[red blue green]
+        end
+      end
+    }}
+
+    its(:as_json) { should == {
+      "name" => "void",
+      "type" => "object",
+      "properties" => {
+        "attributes" => {
+          "type" => "object",
+          "required" => true,
+          "properties" => {
+            "color" => {
+              "type" => "string",
+              "enum" => %w[red blue green]
             }
           }
         }
       }
-    end
+    }}
   end
 end
